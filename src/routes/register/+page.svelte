3ca05b83a24b846c54db9c5c0e7b390ebbc29075
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-08 13:14:18 +0100

    feat(register): with secret only (set via env)

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import Register from '$lib/components/custom/register/register-page.svelte';
+</script>
+
+<form method="POST" use:enhance>
+	<Register />
+</form>
