602a77c71c97889871966e4ecadc72276974059e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-11 03:47:43 +0100

    fix(search): email with substr fuzzy

--- src/lib/server/db/fuzzysearch.ts
+++ src/lib/server/db/fuzzysearch.ts
@@ -2,15 +2,24 @@ import { sql, ilike, type Column, type SQL } from 'drizzle-orm';
 
 const EDIT_DISTANCE = 3;
 
-export function fuzzySearchFilters(dbField: Column, searchQuery: string): SQL[] {
+export function fuzzySearchFilters(
+	dbField: Column,
+	searchQuery: string,
+	substr: boolean = false
+): SQL[] {
 	// Assert dbField is valid
 	if (dbField === null || dbField === undefined) throw new Error('Invalid dbField');
 	// Assert searchQuery is valid
 	if (searchQuery === null || searchQuery === undefined || searchQuery === '')
 		throw new Error('Invalid searchQuery');
 
-	return [
-		sql`LEVENSHTEIN(${dbField}, ${searchQuery}) < ${EDIT_DISTANCE}`,
-		ilike(dbField, `${searchQuery}%`)
-	];
+	return substr
+		? [
+				sql`LEVENSHTEIN(${dbField}, ${searchQuery}) < ${EDIT_DISTANCE}`,
+				ilike(dbField, `%${searchQuery}%`)
+			]
+		: [
+				sql`LEVENSHTEIN(${dbField}, ${searchQuery}) < ${EDIT_DISTANCE}`,
+				ilike(dbField, `${searchQuery}%`)
+			];
 }
