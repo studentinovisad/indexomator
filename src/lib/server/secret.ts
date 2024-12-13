1c3ccf8e8a884d2f3ce9d5b58dc598f9b436746f
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-13 23:13:06 +0100

    fix(secret): const env

--- src/lib/server/secret.ts
+++ src/lib/server/secret.ts
@@ -7,11 +7,12 @@ export async function validateSecret(secret: string): Promise<boolean> {
 		throw new Error('Invalid secret');
 	}
 
+	const secretEnv = env.SECRET;
 	// Assert that the secret env exists
-	if (env.SECRET === null || env.SECRET === undefined || env.SECRET === '') {
+	if (secretEnv === null || secretEnv === undefined || secretEnv === '') {
 		throw new Error('SECRET env is not defined or empty string');
 	}
 
-	const secretHash = await hashPassword(env.SECRET);
+	const secretHash = await hashPassword(secretEnv);
 	return await verifyPasswordHash(secretHash, secret);
 }
