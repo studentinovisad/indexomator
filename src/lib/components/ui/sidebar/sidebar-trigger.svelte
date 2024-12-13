da3b318708184fef323c323580b12d4ded99bcbc
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-13 16:15:41 +0100

    fix: sidebar and format

--- src/lib/components/ui/sidebar/sidebar-trigger.svelte
+++ src/lib/components/ui/sidebar/sidebar-trigger.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils.js";
-	import PanelLeft from "lucide-svelte/icons/panel-left";
-	import type { ComponentProps } from "svelte";
-	import { useSidebar } from "./context.svelte.js";
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+	import PanelLeft from 'lucide-svelte/icons/panel-left';
+	import type { ComponentProps } from 'svelte';
+	import { useSidebar } from './context.svelte.js';
 
 	let {
 		ref = $bindable(null),
@@ -24,9 +24,9 @@
 		sidebar.toggle();
 	}}
 	data-sidebar="trigger"
-	variant="ghost"
+	variant="outline"
 	size="icon"
-	class={cn("h-7 w-7", className)}
+	class={cn('h-7 w-7', className)}
 	{...restProps}
 >
 	<PanelLeft />
