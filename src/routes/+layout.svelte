65a31eecc193cf78b2af311cfe8859cfc3accc3b
Matija Kljajić <64456143+matijakljajic@users.noreply.github.com>
2024-12-17 16:15:42 +0100

    fix: sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from "$lib/components/ui/sonner/index.js";
 	import ThemeToggle from '$lib/components/custom/themetoggle/theme-toggle.svelte';
 
 	let { children } = $props();
 </script>
+
+<Toaster />
 
 <ModeWatcher defaultMode={'dark'} />
 <ThemeToggle />
