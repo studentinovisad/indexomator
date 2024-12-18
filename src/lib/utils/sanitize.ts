03f188402c743b808badcb0431ee91407c44e223
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-18 22:42:29 +0100

    feat(utils): remove diacritics func

--- src/lib/utils/sanitize.ts
+++ src/lib/utils/sanitize.ts
@@ -8,3 +8,25 @@ export function capitalizeString(value: string): string {
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
 		.join(' ');
 }
+
+export function removeDiacritics(input: string): string {
+	const diacriticsMap = new Map([
+		['č', 'c'],
+		['Č', 'C'],
+		['ć', 'c'],
+		['Ć', 'C'],
+		['ǆ', 'dz'],
+		['Ǆ', 'Dz'],
+		['đ', 'dj'],
+		['Đ', 'Dj'],
+		['š', 's'],
+		['Š', 'S'],
+		['ž', 'z'],
+		['Ž', 'Z']
+	]);
+
+	return input
+		.split('')
+		.map((char) => diacriticsMap.get(char) ?? char)
+		.join('');
+}
