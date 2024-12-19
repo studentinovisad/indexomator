2ad0751705115fd48461f3d706439a5157f7a14e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-19 20:13:59 +0100

    fix(search): small fixes

--- src/lib/server/db/fuzzysearch.ts
+++ src/lib/server/db/fuzzysearch.ts
@@ -20,19 +20,26 @@ export function fuzzySearchFilters(
 		throw new Error('Invalid searchQuery');
 	}
 
-	if (opts.substr === null) throw new Error('Invalid substr');
-	if (opts.distance === null || (opts.distance != undefined && opts.distance <= 0))
-		throw new Error('Invalid distance');
+	// Assert substr option is valid
+	if (opts.substr === null) {
+		throw new Error('Invalid substr option');
+	}
+
+	// Assert distance option is valid
+	if (opts.distance === null || (opts.distance !== undefined && opts.distance <= 0)) {
+		throw new Error('Invalid distance option');
+	}
 
 	const concatQuery = dbFields
 		.map((field) => sql`${field}`)
 		.reduce((prev, curr) => sql`${prev} || ' ' || ${curr}`);
 
 	// @ts-expect-error because there is no typedef for sql as first param in ilike function
-	const ilikeFilter = [ilike(concatQuery, `${opts.substr ? '%' : ''}${searchQuery}%`)];
-	const levenshteinFilter = opts.distance !== undefined
-		? [sql`LEVENSHTEIN(LOWER(${concatQuery}), ${searchQuery}) <= ${opts.distance}`]
-		: [];
+	const ilikeFilter = ilike(concatQuery, `${opts.substr ? '%' : ''}${searchQuery}%`);
+	const levenshteinFilter =
+		opts.distance !== undefined
+			? [sql`LEVENSHTEIN(LOWER(${concatQuery}), ${searchQuery}) <= ${opts.distance}`]
+			: [];
 
-	return [...ilikeFilter, ...levenshteinFilter];
+	return [ilikeFilter, ...levenshteinFilter];
 }
