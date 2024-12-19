5b6caa90d3aeb2814b6b6e9f3d8bf434b7bda979
Miša Stefanović <mikmikyt1011@gmail.com>
2024-12-19 02:13:06 +0100

    refactor: merge fuzzy search functions, support for infinite columns

--- src/lib/server/db/fuzzysearch.ts
+++ src/lib/server/db/fuzzysearch.ts
@@ -1,17 +1,17 @@
 import { sql, ilike, type Column, type SQL } from 'drizzle-orm';
 
 type FuzzySearchFiltersOptions = {
-	distance?: number,
-	substr?: boolean
+	distance?: number;
+	substr?: boolean;
 };
 
 export function fuzzySearchFilters(
-	dbField: Column,
+	dbFields: Column[],
 	searchQuery: string,
 	opts: FuzzySearchFiltersOptions = {}
 ): SQL[] {
-	// Assert dbField is valid
-	if (dbField === null || dbField === undefined) {
+	// Assert dbFields are valid
+	if (dbFields === null || dbFields === undefined || dbFields.length === 0) {
 		throw new Error('Invalid dbField');
 	}
 
@@ -20,40 +20,15 @@ export function fuzzySearchFilters(
 		throw new Error('Invalid searchQuery');
 	}
 
-	const ilikeFilter = [ilike(dbField, `${opts.substr ? '%' : ''}${searchQuery}%`)];
-	const levenshteinFilter = opts.distance ? [sql`LEVENSHTEIN(LOWER(${dbField}), ${searchQuery}) <= ${opts.distance}`,] : [];
-	return [
-		...ilikeFilter,
-		...levenshteinFilter
-	];
-}
-
-export function fuzzyConcatSearchFilters(
-	dbField1: Column,
-	dbField2: Column,
-	searchQuery: string,
-	opts: FuzzySearchFiltersOptions = {}
-): SQL[] {
-	// Assert dbField1 is valid
-	if (dbField1 === null || dbField1 === undefined) {
-		throw new Error('Invalid dbField1');
-	}
-
-	// Assert dbField2 is valid
-	if (dbField2 === null || dbField2 === undefined) {
-		throw new Error('Invalid dbField2');
-	}
-
-	// Assert searchQuery is valid
-	if (searchQuery === null || searchQuery === undefined || searchQuery === '') {
-		throw new Error('Invalid searchQuery');
-	}
+	const concatQuery = dbFields
+		.map((field) => sql`${field}`)
+		.reduce((prev, curr) => sql`${prev} || ' ' || ${curr}`);
 
 	// @ts-expect-error because there is no typedef for sql as first param in ilike function
-	const ilikeFilter = [ilike(sql`${dbField1} || ' ' || ${dbField2}`, `${opts.substr ? '%' : ''}${searchQuery}%`)];
-	const levenshteinFilter = opts.distance ? [sql`LEVENSHTEIN(LOWER(${dbField1}) || ' ' || LOWER(${dbField2}), ${searchQuery}) <= ${opts.distance}`] : [];
-	return [
-		...ilikeFilter,
-		...levenshteinFilter
-	];
+	const ilikeFilter = [ilike(concatQuery, `${opts.substr ? '%' : ''}${searchQuery}%`)];
+	const levenshteinFilter = opts.distance
+		? [sql`LEVENSHTEIN(LOWER(${concatQuery}), ${searchQuery}) <= ${opts.distance}`]
+		: [];
+
+	return [...ilikeFilter, ...levenshteinFilter];
 }
