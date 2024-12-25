6a9d20925d4ea524f0dd2c2a17bfd0616540e7bd
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-25 16:19:47 +0100

    fix(search): switch to POST for search and show toast on toggle state

--- src/routes/(dashboard)/+page.server.ts
+++ src/routes/(dashboard)/+page.server.ts
@@ -6,11 +6,10 @@ import { Employee, Student, type Person } from '$lib/types/person';
 import { invalidateSession } from '$lib/server/db/session';
 import { deleteSessionTokenCookie } from '$lib/server/session';
 
-export const load: PageServerLoad = async ({ url }) => {
+export const load: PageServerLoad = async () => {
 	try {
-		const searchQuery = url.searchParams.get('q') ?? undefined;
-		const studentsP = getStudents(1000, 0, searchQuery);
-		const employeesP = getEmployees(1000, 0, searchQuery);
+		const studentsP = getStudents(1000, 0, undefined);
+		const employeesP = getEmployees(1000, 0, undefined);
 		const students = await studentsP;
 		const employees = await employeesP;
 
@@ -38,18 +37,62 @@ export const load: PageServerLoad = async ({ url }) => {
 		];
 
 		return {
-			searchQuery,
 			persons
 		};
 	} catch (err: unknown) {
 		console.debug(`Failed to get students and employees: ${(err as Error).message}`);
 		return fail(500, {
+			persons: [] as Person[],
 			message: 'Failed to get students and employees'
 		});
 	}
 };
 
 export const actions: Actions = {
+	search: async ({ request }) => {
+		try {
+			const formData = await request.formData();
+			const searchQuery = formData.get('q')?.toString() ?? undefined;
+
+			const studentsP = getStudents(1000, 0, searchQuery);
+			const employeesP = getEmployees(1000, 0, searchQuery);
+			const students = await studentsP;
+			const employees = await employeesP;
+
+			const persons: Person[] = [
+				...students.map((s) => ({
+					id: s.id,
+					type: Student,
+					identifier: s.index,
+					fname: s.fname,
+					lname: s.lname,
+					department: s.department,
+					building: s.building,
+					state: s.state
+				})),
+				...employees.map((e) => ({
+					id: e.id,
+					type: Employee,
+					identifier: e.email,
+					fname: e.fname,
+					lname: e.lname,
+					department: e.department,
+					building: e.building,
+					state: e.state
+				}))
+			];
+
+			return {
+				searchQuery,
+				persons
+			};
+		} catch (err: unknown) {
+			console.debug(`Failed to search: ${(err as Error).message}`);
+			return fail(500, {
+				message: 'Failed to search'
+			});
+		}
+	},
 	togglestate: async ({ request, locals }) => {
 		try {
 			const formData = await request.formData();
@@ -86,10 +129,14 @@ export const actions: Actions = {
 			} else if (type === Employee) {
 				await toggleEmployeeState(id, building, creator);
 			} else {
-				return fail(400, {
+				return fail(500, {
 					message: 'Invalid type (neither student nor employee)'
 				});
 			}
+
+			return {
+				message: 'Successfully toggled state'
+			};
 		} catch (err: unknown) {
 			console.debug(`Failed to toggle state: ${(err as Error).message}`);
 			return fail(400, {
