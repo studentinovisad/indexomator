d1e09104781122f96ce89c09371a33935d24cdb1
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-12-31 03:59:21 +0100

    feat!: merge students and employees tables into persons tables

---
+++ drizzle/0006_fearless_photon.sql
@@ -0,0 +1,6 @@
+DROP TABLE "employee_entry";--> statement-breakpoint
+DROP TABLE "employee_exit";--> statement-breakpoint
+DROP TABLE "employee";--> statement-breakpoint
+DROP TABLE "student_entry";--> statement-breakpoint
+DROP TABLE "student_exit";--> statement-breakpoint
+DROP TABLE "student";
--- src/lib/components/custom/inside_counter/insideCountPerBuilding.svelte
+++ src/lib/components/custom/inside_counter/insideCountPerBuilding.svelte
@@ -1,51 +1,26 @@
 <script lang="ts">
 	let {
-		dataStudents,
-		dataEmployees
+		data
 	}: {
-		dataStudents: {
-			building: string;
-			insideCount: number;
-		}[];
-		dataEmployees: {
+		data: {
 			building: string;
 			insideCount: number;
 		}[];
 	} = $props();
-
-	const allBuildings = [
-		...new Set([...dataStudents, ...dataEmployees].map((item) => item.building))
-	];
-	const mergedData = allBuildings.map((building) => {
-		const studentEntry = dataStudents.find((s) => s.building === building);
-		const employeeEntry = dataEmployees.find((e) => e.building === building);
-		if (studentEntry === undefined || employeeEntry === undefined) {
-			throw new Error('Invalid student or employee data');
-			// ^^ Should never happen
-		}
-
-		return {
-			building: building,
-			studentInsideCount: studentEntry.insideCount,
-			employeeInsideCount: employeeEntry.insideCount
-		};
-	});
 </script>
 
 <table class="w-full">
 	<thead>
 		<tr>
 			<th class="w-1/3">Building</th>
-			<th class="w-1/3">Students</th>
-			<th class="w-1/3">Employees</th>
+			<th class="w-1/3">People</th>
 		</tr>
 	</thead>
 	<tbody class="text-center">
-		{#each mergedData as { building, studentInsideCount, employeeInsideCount }}
+		{#each data as { building, insideCount }}
 			<tr>
 				<td>{building}</td>
-				<td>{studentInsideCount}</td>
-				<td>{employeeInsideCount}</td>
+				<td>{insideCount}</td>
 			</tr>
 		{/each}
 	</tbody>
