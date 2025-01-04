511c67b79b4ec3798562147a234b403acaaab297
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-01-04 18:05:18 +0100

    fix(stats): refresh button

--- src/routes/(dashboard)/statistics/+page.server.ts
+++ src/routes/(dashboard)/statistics/+page.server.ts
@@ -1,5 +1,5 @@
 import { getPersonsCountPerBuilding, getPersonsCountPerDepartment } from '$lib/server/db/person';
-import { fail } from '@sveltejs/kit';
+import { fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
@@ -19,5 +19,27 @@ export const load: PageServerLoad = async () => {
 		return fail(500, {
 			message: 'Failed to get persons'
 		});
+	}
+};
+
+export const actions: Actions = {
+	default: async () => {
+		try {
+			const personsCountP = getPersonsCountPerDepartment();
+			const personsInsideCountP = getPersonsCountPerBuilding();
+
+			const personsCount = await personsCountP;
+			const personsInsideCount = await personsInsideCountP;
+
+			return {
+				personsCount,
+				personsInsideCount
+			};
+		} catch (err: unknown) {
+			console.debug(`Failed to get persons: ${(err as Error).message}`);
+			return fail(500, {
+				message: 'Failed to get persons'
+			});
+		}
 	}
 };
--- src/routes/(dashboard)/statistics/+page.svelte
+++ src/routes/(dashboard)/statistics/+page.svelte
@@ -3,14 +3,22 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import CountPerDepartment from '$lib/components/custom/stats/countPerDepartment.svelte';
 	import InsideCountPerBuilding from '$lib/components/custom/stats/insideCountPerBuilding.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { enhance } from '$app/forms';
 
-	let { data } = $props();
+	let { data, form: actionData } = $props();
 
-	const personsCount = $derived(data.personsCount ?? []);
-	const personsInsideCount = $derived(data.personsInsideCount ?? []);
+	const personsCount = $derived(actionData?.personsCount ?? data.personsCount ?? []);
+	const personsInsideCount = $derived(
+		actionData?.personsInsideCount ?? data.personsInsideCount ?? []
+	);
 </script>
 
-<div class="flex w-full items-center justify-center px-4 pt-[5dvh] sm:pt-[10dvh]">
+<form
+	method="POST"
+	class="flex w-full items-center justify-center px-4 pt-[5dvh] sm:pt-[10dvh]"
+	use:enhance
+>
 	<Tabs.Root value="general" class="mx-auto w-full max-w-sm">
 		<Tabs.List class="grid w-full grid-cols-2">
 			<Tabs.Trigger value="general">General</Tabs.Trigger>
@@ -25,6 +33,9 @@
 				<Card.Content>
 					<CountPerDepartment {personsCount} />
 				</Card.Content>
+				<Card.Footer>
+					<Button type="submit" class="mx-auto">Refresh</Button>
+				</Card.Footer>
 			</Card.Root>
 		</Tabs.Content>
 		<Tabs.Content value="inside">
@@ -36,7 +47,10 @@
 				<Card.Content>
 					<InsideCountPerBuilding {personsInsideCount} />
 				</Card.Content>
+				<Card.Footer>
+					<Button type="submit" class="mx-auto">Refresh</Button>
+				</Card.Footer>
 			</Card.Root>
 		</Tabs.Content>
 	</Tabs.Root>
-</div>
+</form>
