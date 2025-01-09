2b2baf536dfff9ae00e3470874754d7e1c51ca7a
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2025-01-09 13:12:49 +0100

    Added animations on cell

--- src/routes/(dashboard)/data-table-editable.svelte
+++ src/routes/(dashboard)/data-table-editable.svelte
@@ -2,6 +2,7 @@
 	import { Input } from '$lib/components/ui/input';
 	import type { Person } from '$lib/types/person';
 	import type { Table } from '@tanstack/table-core';
+	import { fly } from 'svelte/transition';
 
 	let {
 		id,
@@ -34,7 +35,11 @@
 </script>
 
 {#if enabled && table.options !== null && table.options.meta?.getEditChanges(id)}
-	<Input class="w-min field-sizing-content" bind:value={getChangesMap, updateChangesMap} />
+	<div in:fly>
+		<Input class="w-min field-sizing-content" bind:value={getChangesMap, updateChangesMap} />
+	</div>
 {:else}
-	{value}
+	<div in:fly>
+		{value}
+	</div>
 {/if}
