e3abb32def8d7fb29c039aad58b86e4799435f07
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-01-19 15:29:46 +0100

    fix(db): ratelimit table mishaps

--- src/lib/server/db/schema/user.ts
+++ src/lib/server/db/schema/user.ts
@@ -1,4 +1,12 @@
-import { boolean, integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import {
+	boolean,
+	integer,
+	pgTable,
+	primaryKey,
+	serial,
+	text,
+	timestamp
+} from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
 	id: serial('id').primaryKey(),
@@ -6,10 +14,16 @@ export const userTable = pgTable('user', {
 	passwordHash: text('password_hash').notNull()
 });
 
-export const ratelimitTable = pgTable('ratelimit', {
-	userId: integer('user_id')
-		.notNull()
-		.references(() => userTable.id, { onDelete: 'cascade' }),
-	timestamp: timestamp('timestamp').defaultNow().notNull(),
-	lock: boolean('lock').notNull()
-});
+export const ratelimitTable = pgTable(
+	'ratelimit',
+	{
+		userId: integer('user_id')
+			.notNull()
+			.references(() => userTable.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+		timestamp: timestamp('timestamp').defaultNow().notNull(),
+		lock: boolean('lock').notNull()
+	},
+	(table) => ({
+		pk: primaryKey({ columns: [table.userId, table.timestamp] })
+	})
+);
