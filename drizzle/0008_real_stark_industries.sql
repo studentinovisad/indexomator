563eca259b85068915cbfcbfcfef0834204579d4
Aleksandar Sredojević <ferrumite666@gmail.com>
2025-01-29 18:32:22 +0100

    drizzle: reset migrations

--- drizzle/0008_real_stark_industries.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "user" ADD COLUMN "active" boolean DEFAULT true NOT NULL;
--- drizzle/0009_old_tenebrous.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" RENAME COLUMN "active" TO "disabled";--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "disabled" SET DEFAULT false;
