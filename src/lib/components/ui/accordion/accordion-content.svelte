58271dfa2069f7d76e33357cbf1bf4b38da4d719
daliborstakic <stakicdalibor64@gmail.com>
2025-05-24 19:14:20 +0200

    chore(shadcn): fix shadcn compatibility issue with bits-ui

--- src/lib/components/ui/accordion/accordion-content.svelte
+++ src/lib/components/ui/accordion/accordion-content.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-	import { Accordion as AccordionPrimitive } from "bits-ui";
-	import { slide } from "svelte/transition";
-	import { cn } from "$lib/utils.js";
+	import { Accordion as AccordionPrimitive } from 'bits-ui';
+	import { slide } from 'svelte/transition';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = AccordionPrimitive.ContentProps;
 
-	let className: $$Props["class"] = undefined;
-	export let transition: $$Props["transition"] = slide;
-	export let transitionConfig: $$Props["transitionConfig"] = {
-		duration: 200,
-	};
+	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
 <AccordionPrimitive.Content
-	class={cn("overflow-hidden text-sm transition-all", className)}
-	{transition}
-	{transitionConfig}
+	class={cn('overflow-hidden text-sm transition-all', className)}
 	{...$$restProps}
 >
 	<div class="pb-4 pt-0">
--- src/lib/components/ui/accordion/accordion-item.svelte
+++ src/lib/components/ui/accordion/accordion-item.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import { Accordion as AccordionPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+	import { Accordion as AccordionPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
 
-	type $$Props = AccordionPrimitive.ItemProps;
+	import type { ClassValue } from 'clsx';
 
-	let className: $$Props["class"] = undefined;
-	export let value: $$Props["value"];
+	export let value: string;
+	export let className: ClassValue = undefined;
+
 	export { className as class };
 </script>
 
-<AccordionPrimitive.Item {value} class={cn("border-b", className)} {...$$restProps}>
+<AccordionPrimitive.Item {value} class={cn('border-b', className)} {...$$restProps}>
 	<slot />
 </AccordionPrimitive.Item>
